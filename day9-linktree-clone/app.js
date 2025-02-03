5af774146ddf11a89762e63a5f293ce55b45a583
Sonushavik <sonuk06212@gmail.com>
2025-02-03 10:28:36 +0530

    linktree clone

---
+++ day9-linktree-clone/app.js
@@ -0,0 +1,49 @@
+// {
+//         "username": "Sonu kumar",
+//         "bio": "im web dev",
+//         "profileImage": "https://instagram.frdp4-1.fna.fbcdn.net/v/t51.2885-19/475279675_1176261637448756_3977288492898077980_n.jpg?stp=dst-jpg_s320x320_tt6&_nc_ht=instagram.frdp4-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=7sOjmA3O9gUQ7kNvgExVQuv&_nc_gid=801adb32aa2041d7bac9c38caf9d5691&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AYCJkM8iGBxFhJ0WaMEd4UwvihjzX3WNEa_kTOJF_BtP1Q&oe=67A6136F&_nc_sid=8b3546",
+//         "socialLinks": [
+//           {
+//             "linkName": "Linkedin",
+//             "linkUrl": "www.linkedin.com/in/sonushavik"
+//           },
+//           {
+//             "linkName": "Github",
+//             "linkUrl": "https://github.com/Sonushavik/My-GH-Profile"
+//           }
+//         ]
+//       }
+
+const loader = document.querySelector('.loader')
+const card = document.querySelector('.card')
+const username = document.querySelector('.username')
+const bio = document.querySelector('.bio')
+const profileImage = document.querySelector('.profile-image')
+const linkName = document.querySelector('.link-name')
+const linkUrl = document.getElementById('linkUrl')
+const linkCard = document.querySelector('.inner-card')
+
+function applyData(data){
+        username.innerText = data.username;
+        bio.innerText = data.bio;
+        profileImage.src = data.profileImage
+        data.socialLinks.map(link => {
+                linkCard.innerHTML = linkCard.innerHTML + `<a id="linkUrl" class= "link-div" href="${link.linkUrl}">
+                <img src="icons/${link.linkName}.png" alt ="" class="icon">
+                <h2 class = "link-name">${link?.linkName}</h2>
+                </a>`
+        })
+}
+
+function fetchData () {
+        loader.style.display = 'block'
+        fetch('https://api.npoint.io/9447d17b2b34e3f70165')
+        .then(data =>data.json() )
+        .then(data => {
+                loader.style.display = 'none'
+                card.style.display = 'block'
+                applyData(data)
+        })
+}
+
+fetchData()
